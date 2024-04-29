2d1f7f50b8a1d356355fb933a3959b7abfb0e17e
Mohannad Albanayosy <m.banayosi@gmail.com>
2024-04-29 20:16:07 +0200

    add: function to load images

--- dataset.py
+++ dataset.py
@@ -1,6 +1,8 @@
+import os
 import torch
 from PIL import Image
 from torchvision import transforms
+from tqdm import tqdm
 
 
 transform_input = transforms.Compose([
@@ -18,3 +20,19 @@ def introduce_noise(input_image, device, noise_factor=0.3):
     noisy_input_image = input_image + noise_factor * torch.randn(input_image.size()).to(device)
     noisy_input_image = torch.clamp(noisy_input_image, 0, 1).to(device)
     return noisy_input_image
+
+def prepare_dataset(device, base_path):
+    dataset = os.listdir(base_path)
+    training_dataset = []
+
+    with tqdm(total=len(dataset), desc=f"loading files") as pbar:
+        for file_name in dataset:
+            if file_name.split(".")[-1] in ("JPG", "jpeg"):
+                image_path = f"{base_path}/{file_name}"
+                input_image = load_image(image_path).to(device)
+                noisy_input_image = introduce_noise(input_image, device)
+                training_dataset.append((input_image, noisy_input_image))
+            pbar.update(1)
+
+    print(f"{len(training_dataset)=}")
+    return training_dataset
