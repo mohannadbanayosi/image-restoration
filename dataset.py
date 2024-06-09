10fbf4247e82f64edc6a62ace3ddb213c78fbfcc
Mohannad Albanayosy <m.banayosi@gmail.com>
2024-06-09 15:32:12 +0200

    Improved training setup (#5)
    * add: data folder to gitignore
    
    * update: seed now set
    
    For consistent experiments.
    
    * update: template for graph naming
    
    * update: input image resize 16 -> 64
    
    With smaller batches we can have bigger images.
    
    * update: training
    
    1. Using PyTorch `data` module instead of the custom coded
    2. batch size 256 -> 64
    3. lr decreased
    4. Epochs instead of steps
    5. Added followup todos
    
    * remove: path
    
    * fix: issue with calculating the PSNR
    
    Issue detected when trying another implementation of PSNR calculation.
    Last batch could potentially be smaller than batch_size.
    
    * add: ssim as a metric
    
    * update: parallel data loader
    
    * add: use `CIFAR10` dataset
    
    * update: save metadata and final metrics
    
    * add: noise level as config
    
    * refactor: remove redundant code
    
    * refactor: move init code
    
    This way the new processes won't need to execute this block every time they are initialised.

--- dataset.py
+++ dataset.py
@@ -7,11 +7,13 @@ from torchvision import transforms
 from tqdm import tqdm
 
 
+random.seed(42)
+
 training_dataset_index = 0
 epoch = 0
 
 transform_input = transforms.Compose([
-    transforms.Resize((16, 16)),
+    transforms.Resize((64, 64)),
     transforms.ToTensor()
 ])
 
@@ -26,41 +28,6 @@ def introduce_noise(input_image, device, noise_factor=0.3):
     noisy_input_image = torch.clamp(noisy_input_image, 0, 1).to(device)
     return noisy_input_image
 
-def prepare_dataset(device, base_path):
-    dataset = os.listdir(base_path)
-    training_dataset = []
-
-    with tqdm(total=len(dataset), desc=f"loading files") as pbar:
-        for file_name in dataset:
-            if file_name.split(".")[-1] in ("JPG", "jpeg"):
-                image_path = f"{base_path}/{file_name}"
-                input_image = load_image(image_path).to(device)
-                noisy_input_image = introduce_noise(input_image, device)
-                training_dataset.append((input_image, noisy_input_image))
-            pbar.update(1)
-
-    print(f"{len(training_dataset)=}")
-    return training_dataset
-
-def get_batch(batch_size, dataset, selection_mode = "random"):
-    global training_dataset_index, epoch
-    original_images = []
-    noisy_images = []
-    for _ in range(batch_size):
-        if selection_mode == "iterative":
-            training_dataset_index = training_dataset_index % len(dataset)
-            if training_dataset_index == 0:
-                epoch += 1
-            input_image, noisy_input_image = dataset[training_dataset_index]
-            training_dataset_index += 1
-        elif selection_mode == "random":
-            input_image, noisy_input_image = random.choice(dataset)
-        else:
-            raise ValueError(f"Incorrect value set for selection_mode: {selection_mode}")
-
-        original_images.append(input_image)
-        noisy_images.append(noisy_input_image)
-    return original_images, noisy_images
 
 def calculate_psnr(base_image, modified_image):
     """Peak Signal-to-Noise Ratio"""
--- train.py
+++ train.py
@@ -1,69 +1,82 @@
+# TODO: fix ordering of the imports
+import json
 import time
 from matplotlib import pyplot as plt
 import numpy as np
 import torch
 import torch.nn as nn
+from torch.utils.data import DataLoader, random_split
+from torchvision import datasets
 from tqdm import tqdm
-from dataset import calculate_psnr, get_batch, prepare_dataset
+from dataset import calculate_psnr, transform_input
 from model import DenoisingAutoencoder
+from torchmetrics.image import StructuralSimilarityIndexMeasure
 
-# Hyperparameters
-batch_size = 256
-learning_rate = 0.0005
+# Hyperparameters and config
+batch_size = 64
+learning_rate = 0.00001
 max_iters = 20000
 eval_interval = int(max_iters/20)
 eval_iters = 20
+num_workers = 2
+num_epochs = 3
+noise_level = 0.1
 
-device = "mps" if torch.backends.mps.is_available() else "cpu"
-model = DenoisingAutoencoder().to(device)
-print(sum(p.numel() for p in model.parameters())/1e6, 'M parameters')
-
-# TODO: check possibility to switch to perceptual or adversarial loss
-criterion = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-
-dataset = prepare_dataset(device, "IMAGES_PATH")
-n = int(0.9*len(dataset))
-training_dataset = dataset[:n]
-validation_dataset = dataset[n:]
-print(f"{len(training_dataset)=}", f"{len(validation_dataset)=}")
 
 @torch.no_grad()
 def calculate_batch_psnr(batch_input_images, output_image):
-    psnrs = torch.zeros(batch_size)
+    psnrs = torch.zeros(batch_input_images.size(0))
     for i in range(len(batch_input_images)):
         input_img_np = batch_input_images[i].cpu().numpy().transpose((1, 2, 0))
         output_img_np = output_image[i].cpu().numpy().transpose((1, 2, 0))
         psnrs[i] = calculate_psnr(input_img_np, output_img_np)
-    return psnrs.mean()
+    return psnrs.mean().numpy()
+
+def save_metadata(filename="config_metadata.json"):
+    # TODO: improve and get rid of hard-coded values where possible
+    config = {
+        "dataset": {
+            "name": "CIFAR10",
+            "root_dir": "/data",
+            "split": "train",
+            "transform": {}
+        },
+        "dataloader": {
+            "batch_size": batch_size,
+            "shuffle": True,
+            "num_workers": num_workers
+        },
+        "model": {
+            "architecture": "DenoisingAutoencoder",
+            "noise_level": noise_level,
+            "encoder": [
+                {"in_channels": 3, "out_channels": 64, "kernel_size": 3, "stride": 1, "padding": 1},
+                {"in_channels": 64, "out_channels": 128, "kernel_size": 3, "stride": 2, "padding": 1},
+                {"in_channels": 128, "out_channels": 256, "kernel_size": 3, "stride": 2, "padding": 1}
+            ],
+            "decoder": [
+                {"in_channels": 256, "out_channels": 128, "kernel_size": 4, "stride": 2, "padding": 1},
+                {"in_channels": 128, "out_channels": 64, "kernel_size": 4, "stride": 2, "padding": 1},
+                {"in_channels": 64, "out_channels": 3, "kernel_size": 2, "stride": 1, "padding": 1}
+            ]
+        },
+        "training": {
+            "num_epochs": num_epochs,
+            "loss_function": "MSELoss",
+            "optimizer": "Adam",
+            "learning_rate": learning_rate
+        },
+        "device": str(device)
+    }
+    with open(f"model_resources/{filename}", 'w') as f:
+        json.dump(config, f, indent=4)
 
 
-@torch.no_grad()
-def estimate_loss():
-    final_losses = {}
-    final_psnrs = {}
-    model.eval()
+def save_final(final_res, filename="config_metrics.json"):
+    with open(f"model_resources/{filename}", 'w') as f:
+        json.dump(final_res, f, indent=4)
 
-    for split in ["train", "val"]:
-        losses = torch.zeros(eval_iters)
-        psnrs = torch.zeros(eval_iters)
-        for k in range(eval_iters):
-            original_images, noisy_images = get_batch(batch_size, training_dataset if split == "train" else validation_dataset, selection_mode="random")
-            batch_input_images = torch.stack(original_images)
-            batch_noisy_images = torch.stack(noisy_images)
-            output_image = model(batch_noisy_images)
-            loss = criterion(output_image, batch_input_images)
-            psnr = calculate_batch_psnr(batch_input_images, output_image)
-            losses[k] = loss.item()
-            psnrs[k] = psnr
-        final_losses[split] = losses.mean()
-        final_psnrs[split] = psnrs.mean()
-
-    model.train()
-    return final_losses, final_psnrs
-
-with tqdm(total=max_iters, desc=f"{batch_size=}") as pbar:
-    plots = {
+plots = {
         "loss": {
             "training": [],
             "validation": []
@@ -71,49 +84,116 @@ with tqdm(total=max_iters, desc=f"{batch_size=}") as pbar:
         "psnr": {
             "training": [],
             "validation": []
+        },
+        "ssim": {
+            "training": [],
+            "validation": []
         }
     }
 
-    for iter in range(max_iters):
-        original_images, noisy_images = get_batch(batch_size, training_dataset, selection_mode="iterative")
+def train_model(model, dataloader, valloader, criterion, optimizer, num_epochs=25):
+    # TODO: use already implemented function to add noise instead of duplicating the code here
+    for epoch in range(num_epochs):
+        model.train()
+        running_loss = 0.0
+        running_loss_val = 0.0
+        running_psnrs = 0.0
+        running_psnrs_val = 0.0
+        running_ssim = 0.0
+        running_ssim_val = 0.0
+        for inputs, _ in tqdm(dataloader):
+            inputs = inputs.to(device)
+            noisy_inputs = inputs + noise_level * torch.randn_like(inputs)
+            noisy_inputs = torch.clamp(noisy_inputs, 0., 1.)
 
-        batch_input_images = torch.stack(original_images)
-        batch_noisy_images = torch.stack(noisy_images)
+            optimizer.zero_grad()
+            outputs = model(noisy_inputs)
+            loss = criterion(outputs, inputs)
+            loss.backward()
+            optimizer.step()
 
-        output_image = model(batch_noisy_images)
+            ssim_value = ssim(outputs, inputs)
+            
+            running_loss += loss.item() * inputs.size(0)
+            psnrs = calculate_batch_psnr(inputs, outputs)
+            running_psnrs += psnrs * inputs.size(0)
+            running_ssim += ssim_value.item() * inputs.size(0)
 
-        loss = criterion(output_image, batch_input_images)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        
+        for inputs, _ in tqdm(valloader):
+            model.eval()
+            inputs = inputs.to(device)
+            noisy_inputs = inputs + noise_level * torch.randn_like(inputs)
+            noisy_inputs = torch.clamp(noisy_inputs, 0., 1.)
 
-        if iter % eval_interval == 0:
-            print(f"Step {iter}: Training Loss:", loss.item())
-            calculate_batch_psnr(batch_input_images, output_image)
-            losses, psnrs = estimate_loss()
-            print(f"step {iter}: train loss {losses['train']:.5f}, val loss {losses['val']:.5f}")
-            print(f"step {iter}: train psnr  {psnrs['train']:.2f}, val psnr {psnrs['val']:.2f}")
-            plots["loss"]["training"].append(losses["train"])
-            plots["loss"]["validation"].append(losses["val"])
-            plots["psnr"]["training"].append(psnrs["train"])
-            plots["psnr"]["validation"].append(psnrs["val"])
-        pbar.update(1)
+            outputs = model(noisy_inputs)
+            loss = criterion(outputs, inputs)
 
-print("Final Training Loss:", loss.item())
+            ssim_value = ssim(outputs, inputs)
+            
+            running_loss_val += loss.item() * inputs.size(0)
+            psnrs = calculate_batch_psnr(inputs, outputs)
+            running_psnrs_val += psnrs * inputs.size(0)
+            running_ssim_val += ssim_value.item() * inputs.size(0)
+        
+        epoch_loss = running_loss / len(dataloader.dataset)
+        epoch_loss_val = running_loss_val / len(valloader.dataset)
+        epoch_psnr = running_psnrs / len(dataloader.dataset)
+        epoch_psnr_val = running_psnrs_val / len(valloader.dataset)
+        epoch_ssim = running_ssim / len(dataloader.dataset)
+        epoch_ssim_val = running_ssim_val / len(valloader.dataset)
+        print(f'Epoch {epoch}/{num_epochs - 1}, Loss: {epoch_loss:.4f}, PSNR: {epoch_psnr:.4f}, SSIM: {epoch_ssim:.4f}')
+        print(f'Epoch val {epoch}/{num_epochs - 1}, Loss: {epoch_loss_val:.4f}, PSNR: {epoch_psnr_val:.4f}, SSIM: {epoch_ssim_val:.4f}')
+        plots["loss"]["training"].append(epoch_loss)
+        plots["loss"]["validation"].append(epoch_loss_val)
+        plots["psnr"]["training"].append(epoch_psnr)
+        plots["psnr"]["validation"].append(epoch_psnr_val)
+        plots["ssim"]["training"].append(epoch_ssim)
+        plots["ssim"]["validation"].append(epoch_ssim_val)
 
-timestamp = int(time.time())
+if __name__ == '__main__':
+    timestamp = int(time.time())
+    print(f"model will be saved under {timestamp}")
 
-for plot_type in ("loss", "psnr"):
-    y_training = plots[plot_type]["training"]
-    y_validation = plots[plot_type]["validation"]
-    plt.plot(y_training, label = "Training")
-    plt.plot(y_validation, label = "Validation")
-    plt.ylabel(plot_type)
-    plt.legend()
-    plt.savefig(f"graphs/{plot_type}_{timestamp}.png")
-    plt.clf()
+    torch.manual_seed(1337)
 
-final_loss = "{:.6f}".format(loss.item()).replace(".", "")
-model_resource_path = f"model_resources/model_image_{timestamp}_{final_loss}.pth"
-print(f"Saving model under {model_resource_path}")
-torch.save(model.state_dict(), model_resource_path)
+    device = "mps" if torch.backends.mps.is_available() else "cpu"
+    model = DenoisingAutoencoder().to(device)
+    print(sum(p.numel() for p in model.parameters())/1e6, 'M parameters')
+
+    # TODO: check possibility to switch to perceptual or adversarial loss
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    ssim = StructuralSimilarityIndexMeasure(data_range=1.0).to(device)
+
+    # image_dataset = datasets.ImageFolder(root="IMAGES_PATH", transform=transform_input)
+    train_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_input)
+    val_dataset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_input)
+
+    # train_size = int(0.9 * len(image_dataset))
+    # val_size = len(image_dataset) - train_size
+    # train_dataset, val_dataset = random_split(image_dataset, [train_size, val_size])
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    test_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    print(f"{len(train_loader.dataset)=}", f"{len(test_loader.dataset)=}")
+    
+    save_metadata(f"{timestamp}_metadata.json")
+    
+    train_model(model, train_loader, test_loader, criterion, optimizer, num_epochs=num_epochs)
+
+    save_final(plots, f"{timestamp}_metrics.json")
+
+    for plot_type in ("loss", "psnr", "ssim"):
+        y_training = plots[plot_type]["training"]
+        y_validation = plots[plot_type]["validation"]
+        plt.plot(y_training, label = "Training")
+        plt.plot(y_validation, label = "Validation")
+        plt.ylabel(plot_type)
+        plt.legend()
+        plt.savefig(f"graphs/{timestamp}_{plot_type}.png")
+        plt.clf()
+
+    model_resource_path = f"model_resources/{timestamp}.pth"
+    print(f"Saving model under {model_resource_path}")
+    torch.save(model.state_dict(), model_resource_path)
