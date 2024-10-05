7ee47925ebbbc34da8ee82c9d7dccfb10bbef02c
Mohannad Albanayosy <m.banayosi@gmail.com>
2024-10-05 17:10:24 +0200

    add: SSIM for generation

--- evaluate.py
+++ evaluate.py
@@ -2,10 +2,11 @@ import torch
 from dataset import calculate_psnr, introduce_noise, load_image
 from model import DenoisingAutoencoder
 from torchvision import transforms
+from torchmetrics.image import StructuralSimilarityIndexMeasure
 
 
 # device = "mps" if torch.backends.mps.is_available() else "cpu"
-device = "cpu"
+device = "mps"
 
 base_path = "IMAGES_PATH"
 image_path = f"{base_path}/IMAGE_NAME.JPG"
@@ -40,10 +41,13 @@ with torch.no_grad():
 
 output_image_resized = output_image.squeeze(0)
 
-input_img_np = input_image.squeeze(0).numpy().transpose((1, 2, 0))
-output_img_np = output_image.squeeze(0).numpy().transpose((1, 2, 0))
+input_img_np = input_image.cpu().squeeze(0).numpy().transpose((1, 2, 0))
+output_img_np = output_image.cpu().squeeze(0).numpy().transpose((1, 2, 0))
+ssim = StructuralSimilarityIndexMeasure(data_range=1.0).to(device)
 psnr = calculate_psnr(input_img_np, output_img_np)
 print(f"Image: PSNR = {psnr:.2f}")
+ssim_value = ssim(input_image, output_image)
+print(f"Image: SSIM = {ssim_value.item():.2f}")
 
 output_path = f'{base_path}/output_denoised.jpg'
 transform_output(output_image_resized).save(output_path)
