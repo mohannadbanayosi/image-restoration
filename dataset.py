b48881be90669bb999f1943c3daf9362b7056830
Mohannad Albanayosy <m.banayosi@gmail.com>
2024-10-06 12:53:05 +0200

    Fix metrics and noise (#8)
    * add: SSIM for generation
    
    * fix: noise - needed to be scaled
    
    * fix: psnr calculation

--- dataset.py
+++ dataset.py
@@ -12,6 +12,12 @@ random.seed(42)
 training_dataset_index = 0
 epoch = 0
 
+def introduce_noise(input_image, device="cpu", noise_factor=30):
+    scaled_noise_factor = noise_factor / 255.0
+    noisy_input_image = input_image + scaled_noise_factor * torch.randn_like(input_image).to(device)
+    noisy_input_image = torch.clamp(noisy_input_image, 0, 1).to(device)
+    return noisy_input_image
+
 transform_input = transforms.Compose([
     transforms.Resize((64, 64)),
     transforms.ToTensor()
@@ -24,17 +30,11 @@ def load_image(image_path):
     return image, original_size
 
 
-def introduce_noise(input_image, device, noise_factor=0.3):
-    noisy_input_image = input_image + noise_factor * torch.randn(input_image.size()).to(device)
-    noisy_input_image = torch.clamp(noisy_input_image, 0, 1).to(device)
-    return noisy_input_image
-
-
 def calculate_psnr(base_image, modified_image):
     """Peak Signal-to-Noise Ratio"""
     mse = np.mean((base_image - modified_image) ** 2)
     if mse == 0:
         return float('inf')
     max_pixel = 1.0
-    psnr = 20 * np.log10(max_pixel / np.sqrt(mse))
+    psnr = 10 * np.log10(max_pixel ** 2 / mse)
     return psnr
