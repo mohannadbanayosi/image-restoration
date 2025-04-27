f827af68e1e7577c0fb2866cd8ecd7d1392a589e
Mohannad Albanayosy <m.banayosi@gmail.com>
2025-04-27 18:31:55 +0200

    add: file name to the output file name

--- evaluate.py
+++ evaluate.py
@@ -10,7 +10,8 @@ torch.manual_seed(43)
 device = "mps"
 
 base_path = "IMAGES_PATH"
-image_path = f"{base_path}/IMAGE_NAME.JPG"
+file_name = "FILE_NAME"
+image_path = f"{base_path}/{file_name}.jpeg"
 input_image, original_size = load_image(image_path)
 original_width, original_height = original_size
 input_image = input_image.unsqueeze(0).to(device)
@@ -22,7 +23,9 @@ transform_output = transforms.Compose([
     transforms.ToPILImage()
 ])
 
-output_path = f'{base_path}/output_noised.jpg'
+model_directory = "PATH"
+model_path = "MODEL"
+output_path = f'{base_path}/{file_name}_noised_{model_path.split(".")[0]}.jpg'
 noisy_image_resized = noisy_input_image.squeeze(0)
 transform_output(noisy_image_resized).save(output_path)
 
@@ -30,8 +33,6 @@ model = DenoisingAutoencoder().to(device)
 
 print(sum(p.numel() for p in model.parameters())/1e6, 'M parameters')
 
-model_directory = "/Users/mohannadalbanayosy/Projects/mohannadbanayosi/image-restoration/model_resources"
-model_path = "1718556848.pth"
 print(f"Loading model from {model_directory}/{model_path}")
 model.load_state_dict(torch.load(f"{model_directory}/{model_path}"))
 model.eval()
@@ -50,7 +51,7 @@ print(f"Image: PSNR = {psnr:.2f}")
 ssim_value = ssim(input_image, output_image)
 print(f"Image: SSIM = {ssim_value.item():.2f}")
 
-output_path = f'{base_path}/output_denoised_{model_path.split(".")[0]}.jpg'
+output_path = f'{base_path}/{file_name}_denoised_{model_path.split(".")[0]}.jpg'
 transform_output(output_image_resized).save(output_path)
 
 print("Denoised output image saved at:", output_path)
